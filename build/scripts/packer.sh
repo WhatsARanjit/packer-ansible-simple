0aafb4d43ca7a2205e8d6318b1d89a74532f01da
WhatsARanjit <whatsaranjit@gmail.com>
2022-02-06 12:36:14 -0500

    Added HCP fingerprint

--- build/scripts/packer.sh
+++ build/scripts/packer.sh
@@ -2,6 +2,7 @@
 
 cd packer_files/
 
+export HCP_PACKER_BUILD_FINGERPRINT="$(date +%s)-$(git hash-object aws-linux.pkr.hcl | cut -c -25)"
 packer build \
   -var "owner=$OWNER" \
   -var "version=$VERSION" \
